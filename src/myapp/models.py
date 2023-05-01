a6877c9cf298e627d82a8a9883e353870fbde120
Arian <dervishaj@iMacdeaistrator.orgex.po>
2023-05-01 12:37:41 +0200

    test

--- src/myapp/models.py
+++ src/myapp/models.py
@@ -2,15 +2,31 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Items(models.Model):
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Type(models.Model):
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
+
+
+class Item(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
-    type = models.CharField(max_length=25)
     location = models.CharField(max_length=255)
-    image = models.ImageField()
+    image = models.ImageField(default=None, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    type = models.ForeignKey(Type, on_delete=models.CASCADE)
 
-
-class Categories(models.Model):
-    name = models.CharField(max_length=255)
+    def __str__(self):
+        return self.name
--- src/myapp/views.py
+++ src/myapp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-
+from .models import Item
 # Create your views here.
+def index(request):
+    latest_items_list = Item.objects.order_by("-created_at")
+    context = {"latest_items_list": latest_items_list}
+    render(request, "../templates/index.html", context)
+
 def homepage(request):
     return render(request, "homepage.html")
