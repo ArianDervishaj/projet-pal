1f2e09acf971d295211dde82f82f6e64dd3caf6f
Arian <dervishaj@iMacdeaistrator.orgex.po>
2023-05-03 17:33:06 +0200

    debut chat

--- src/chats/models.py
+++ src/chats/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from myapp.models import Item
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Chat(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    users = models.ManyToManyField(User)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Message(models.Model):
+    content = models.TextField()
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class ChatMember(models.Model):
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # other fields for chat member details
+
+    class Meta:
+        unique_together = ('chat', 'user')
+        # make sure a user can only be a member of a chat once
--- src/chats/views.py
+++ src/chats/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Chat
+from django.contrib.auth.models import User
 # Create your views here.
+def index(request):
+    user = User.objects.get(id=request.user.id)
+    latest_chats = Chat.objects.filter()
