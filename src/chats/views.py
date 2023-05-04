29fc7f1133e183172fd524d5dc6f5871788a9fa2
Arian <dervishaj@iMacdeaistrator.orgex.po>
2023-05-04 10:04:44 +0200

    affichage des discussions et messages

--- src/chats/views.py
+++ src/chats/views.py
@@ -1,7 +1,21 @@
+from django.http import HttpResponseNotAllowed
 from django.shortcuts import render
 from .models import Chat
 from django.contrib.auth.models import User
 # Create your views here.
+
+
 def index(request):
-    user = User.objects.get(id=request.user.id)
-    latest_chats = Chat.objects.filter()
+    if request.user:
+        user_chats = Chat.objects.filter(participants=request.user)
+        context = {
+            'chats': user_chats
+        }
+        return render(request, 'chats/index.html', context)
+    else:
+        return HttpResponseNotAllowed()
+
+
+def detail(request, id):
+    chat = Chat.objects.get(id=id)
+    return render(request, "chats/detail.html", {"chat": chat})
