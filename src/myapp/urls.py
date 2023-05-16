20d54d6d7026e6fb3a323bcbee7bf80cdab961aa
Arian <dervishaj@iMacdeaistrator.orgex.po>
2023-05-16 15:42:46 +0200

    changement dans une url

--- src/myapp/urls.py
+++ src/myapp/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="item-index"),
     path("<int:id>", views.detail, name="item-detail"),
-    path("create/", views.create_item, name="item-create"),
+    path("item-create/", views.create_item, name="item-create"),
     path('delete/<int:item_id>/', views.delete_item, name='delete-item'),
     path('item/<int:item_id>/edit/', views.edit_item, name='item-edit'),
     path('item/<int:item_id>/update/', views.update_item, name='update-item'),
